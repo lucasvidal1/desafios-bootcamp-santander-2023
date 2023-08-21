140c1dcc80e9697d3e42e41c19a4a29e46c05785
lucas.vidal <lucas.vidal021@gmail.com>
2023-08-21 06:08:28 -0300

    Entrega desafio Controle de fluxo

---
+++ desafio-controle-fluxo/src/Exception/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package Exception;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+    }
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
